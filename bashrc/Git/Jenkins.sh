b8e2e74729be8d4b1b856fb0a9ed22c54f6fe631
Cyprien Quilici <quilicicf@gmail.com>
2017-10-03 18:02:09 +0200

    :new: Add local Jenkins launcher (#3)
    * :new: Add local Jenkins launcher
    
    * :eyes: Code review

--- bashrc/Git/Jenkins.sh
+++ bashrc/Git/Jenkins.sh
@@ -9,6 +9,18 @@ test -f "$JENKINS_FILE" || {
   printf '{\n  "login": "",\n  "password": ""\n}\n' > "$JENKINS_FILE"
 }
 
+# Launches a local copy of Jenkins 2
+jenkins() {
+  local logFile
+  logFile="$(mktemp -t "jenkins_logs_XXX_$(date +"%Y_%m_%dT%H_%M_%S")")"
+
+  printf 'Log file is: %b%s%b\n' "$CYAN" "$logFile" "$DEFAULT"
+  pushd "$WORK"
+    nohup java -jar jenkins.war > "$logFile" 2>&1 &
+    xo http://localhost:8080
+  popd
+}
+
 # Opens jenkins page to launch an automated test suite. Copies the current branche's name to the clipboard so that one just has to copy it in jenkins.
 # Uses: cpb, xo, repoJob, isNotNull
 gjenks() {
