59cac6d1c28e2466ae89d771b00db7ad39387f21
quilicicf <cquilici@restlet.com>
2018-03-05 16:39:25 +0100

    :up: Improve netinfo

--- bashrc/Utils/Network.sh
+++ bashrc/Utils/Network.sh
@@ -4,14 +4,16 @@
 
 # Displays information about the network configuration
 netinfo() {
-  printf '--------------- Network Information ---------------\n'
-  /sbin/ifconfig | awk /'inet addr/ {print $2}'
-  /sbin/ifconfig | awk /'Bcast/ {print $3}'
-  /sbin/ifconfig | awk /'inet addr/ {print $4}'
-  /sbin/ifconfig | awk /'HWaddr/ {print $4,$5}'
-  myIP=$(wget -qO- http://ipecho.net/plain)
-  printf 'Public: %s\n' "$myIP"
-  printf '---------------------------------------------------'
+  local localIp publicIp
+  printf '%s\n' '--------------- Network Information ---------------'
+
+  localIp="$(ifconfig | awk '/inet 192\.168\.[0-9.]+  netmask [0-9.]+  broadcast [0-9.]+/ {print $2}')"
+  printf 'Local IP: %s\n' "$localIp"
+
+  publicIp="$(wget -qO- http://ipecho.net/plain)"
+  printf 'Public IP: %s\n' "$publicIp"
+
+  printf '%s\n' '---------------------------------------------------'
 }
 
 # Re-routes a port locally
