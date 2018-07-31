ceac0d588e8cbe781bcb36c27da9c0790054cf65
quilicicf <cquilici@restlet.com>
2018-07-31 10:16:57 +0200

    :new: Add nodePrepend method

---
+++ bashrc/Node.sh
@@ -0,0 +1,9 @@
+#--------#
+# NodeJS #
+#--------#
+
+# Set scripts-prepend-node-path to true globally for the
+# current version of node
+nodePrepend() {
+  npm config set scripts-prepend-node-path true
+}
