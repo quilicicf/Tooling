1825e1ee2f19009235e37313d0d76373d9413e90
quilicicf <cquilici@restlet.com>
2018-10-26 08:46:10 +0200

    :up: Update scrape FFVB script

--- scrape_ffvb/icsify.js
+++ scrape_ffvb/icsify.js
@@ -6,10 +6,15 @@
 * This is a WIP, the package is not published to NPM yet but already available at https://github.com/quilicicf/js2ics
 * The scraping is done via http://webscraper.io/. If you are a french volleyball player you can import sitemap.json
 * into web scraper and change it to match your division.
+*
+* $1: Team name
+* $2: Input file path
+* $3: Output file path
 */
 
 const _ = require('lodash');
 const fs = require('fs');
+const csvtojson = require('csvtojson');
 const moment = require('moment');
 
 const ics = require('../../js2ics/index.js');
@@ -19,11 +24,11 @@ const reformatEntry = (teamName, entry) => {
   const enemyTeam = entry.teamA === teamName
     ? entry.teamB
     : entry.teamA;
-  reformatted.eventName = 'Match contre ' + enemyTeam;
+  reformatted.eventName = `Match contre ${enemyTeam}`;
 
   const splitDate = _.split(entry.date, '/');
   const beginningHour = _.split(entry.time, ':')[ 0 ];
-  const endHour = parseInt(beginningHour) + 3;
+  const endHour = parseInt(beginningHour, 0) + 3;
   const minutes = _.split(entry.time, ':')[ 1 ];
 
   const beginningAsString = `20${splitDate[ 2 ]}${splitDate[ 1 ]}${splitDate[ 0 ]}T${beginningHour}${minutes}`;
@@ -37,23 +42,15 @@ const reformatEntry = (teamName, entry) => {
   return reformatted;
 };
 
-const icsify = (teamName, inputFilePath, outputFilePath) => {
-  const csv = require('csvtojson');
-  const entries = [];
-  csv()
-    .fromFile(inputFilePath)
-    .on('json', (entry) => {
-      entries.push(entry);
-    })
-    .on('end', () => {
-      const events = _(entries)
-        .filter((entry) => entry.teamA === teamName || entry.teamB === teamName)
-        .map(entry => reformatEntry(teamName, entry))
-        .value();
+const icsify = async (teamName, inputFilePath, outputFilePath) => {
+  const json = await csvtojson().fromFile(inputFilePath);
+  const events = _(json)
+    .filter(entry => [ entry.teamA, entry.teamB ].includes(teamName))
+    .map(entry => reformatEntry(teamName, entry))
+    .value();
 
-      const calendar = ics.getCalendar({ events: events });
-      fs.writeFileSync(outputFilePath, calendar, 'utf8');
-    });
+  const calendar = ics.getCalendar({ events });
+  fs.writeFileSync(outputFilePath, calendar, 'utf8');
 };
 
 icsify(process.argv[ 2 ], process.argv[ 3 ], process.argv[ 4 ]);
