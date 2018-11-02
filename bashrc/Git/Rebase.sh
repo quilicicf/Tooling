8fbfe97c400baa058ce40649133c3c8541174dff
quilicicf <cquilici@restlet.com>
2018-11-02 16:01:06 +0100

    :mute: Remove gedit traces from grbn

--- bashrc/Git/Rebase.sh
+++ bashrc/Git/Rebase.sh
@@ -12,7 +12,7 @@ grb() {
 # Makes an interactive rebase (with autosquash) on the n last commits
 # $1: the number of commits to rebase
 grbn() {
-  git rebase "HEAD~$1" -i --autosquash
+  git rebase "HEAD~$1" -i --autosquash 2> /dev/null
 }
 
 # Makes an interactive rebase (with autosquash) on the 2 last commits
