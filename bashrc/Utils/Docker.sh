23be91a28045640c712fa05b53fbf0625bdedff5
quilicicf <cquilici@restlet.com>
2019-01-28 13:05:56 +0100

    :new: Add script to kill docker images

--- bashrc/Utils/Docker.sh
+++ bashrc/Utils/Docker.sh
@@ -32,7 +32,12 @@ dockerRm() {
     local images=()
     while read -r image; do
       images+=("$image")
-    done <<< "$(docker ps -a --filter status=exited | tail -n +1)"
+    done <<< "$(docker ps -a --filter status=exited | tail -n +2)"
+
+    [[ ${#images} -lt 1 ]] && {
+      printfc 'Nothing to remove\n' "$GREEN"
+      return 0
+    }
 
     for image in "${images[@]}"; do
       containerId="$(awk '{print $1}' <<< "$image")"
@@ -44,6 +49,54 @@ dockerRm() {
         docker rm "$containerId"
       else
         printfc "Container not deleted\n" "$YELLOW"
+      fi
+    done
+  fi
+}
+
+dockerKill() {
+  local image containerId name status
+
+  if grep --quiet '\-a' <<< "$*"; then
+    printfc 'Removing all exited containers\n' "$CYAN"
+    while read -r image; do
+      containerId="$(awk -F'|' '{print $1}' <<< "$image")"
+      name="$(awk -F'|' '{print $3}' <<< "$image")"
+      status="$(awk -F'|' '{print $4}' <<< "$image")"
+      local fullContainerId="$name:$containerId"
+
+      if [[ "$status" = Up* ]]; then
+        printfc "Container $fullContainerId killed\n" "$GREEN"
+        docker kill "$containerId"
+
+      else
+        printfc "Image $fullContainerId not running\n" "$YELLOW"
+
+      fi
+    done <<< "$(_parseableDockerPs)"
+
+  else
+    printfc 'Killing running containers in interactive mode\n' "$CYAN"
+    local images=()
+    while read -r image; do
+      images+=("$image")
+    done <<< "$(docker ps -a --filter status=running | tail -n +2)"
+
+    [[ ${#images} -lt 1 ]] && {
+      printfc 'Nothing to kill\n' "$GREEN"
+      return 0
+    }
+
+    for image in "${images[@]}"; do
+      containerId="$(awk '{print $1}' <<< "$image")"
+      printf '%s\n' "$image"
+      read -p "Stop container (y/N)? " -n 1 -r
+      printf '\n'
+      if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+        printfc "Container killed\n" "$GREEN"
+        docker kill "$containerId"
+      else
+        printfc "Container not killed\n" "$YELLOW"
       fi
     done
   fi
