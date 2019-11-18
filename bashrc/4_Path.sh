19d2b9fe50edde3d9deb3f33feb83b9e4a234117
quilicicf <quilicicf@gmail.com>
2019-11-18 13:38:29 +0100

    :shower: Clean path

--- bashrc/4_Path.sh
+++ bashrc/4_Path.sh
@@ -8,9 +8,6 @@ export PATH="$PATH:$WORK/dse/bin/"
 # MongoDB
 export PATH="$WORK/mongodb/bin:$PATH"
 
-# Node
-export PATH="$PATH:/usr/bin/node"
-
 # Maven
 export PATH="$PATH:$MAVEN/bin"
 
@@ -18,16 +15,3 @@ export PATH="$PATH:$MAVEN/bin"
 export PATH="$PATH:/usr/local/go/bin"
 export GOPATH="$HOME/go"
 export PATH="$PATH:$GOPATH/bin"
-
-# NVM
-export NVM_DIR="$HOME/.nvm"
-_configureNvm() {
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-}
-
-_configureNvm
-
-# rbenv
-# export PATH="$HOME/.rbenv/bin:$PATH"
-# eval "$(rbenv init -)"
