05f60340f2b95e590039a44c2c9facb6d6b3bd8d
quilicicf <quilicicf@gmail.com>
2020-02-12 16:58:26 +0100

    :new: Add alias for mkdir

--- bashrc/3_Alias.sh
+++ bashrc/3_Alias.sh
@@ -2,20 +2,20 @@
 #         APT          #
 ########################
 
-alias apti="sudo apt-get install"
-alias apts="sudo apt-cache search"
-alias aptr="sudo apt-get remove"
-alias aptud="sudo apt-get update"
-alias aptug="sudo apt-get upgrade"
-alias aptc="sudo apt-get clean"
-alias aptac="sudo apt-get autoclean"
-alias aptar="sudo apt-get autoremove"
+alias apti='sudo apt-get install'
+alias apts='sudo apt-cache search'
+alias aptr='sudo apt-get remove'
+alias aptud='sudo apt-get update'
+alias aptug='sudo apt-get upgrade'
+alias aptc='sudo apt-get clean'
+alias aptac='sudo apt-get autoclean'
+alias aptar='sudo apt-get autoremove'
 
 #######################
 #     Fools guard     #
 #######################
 
-alias rm="rm -viI --preserve-root"
+alias rm='rm -viI --preserve-root'
 
 alias chown='chown --preserve-root'
 alias chmod='chmod --preserve-root'
@@ -27,10 +27,11 @@ alias wget='wget -c'
 #   Utility aliases   #
 #######################
 
-alias diff="colordiff"
+alias diff='colordiff'
 alias path='echo -e ${PATH//:/\\n}'
-alias now='date +"%T"'
-alias nowdate='date +"%d-%m-%Y"'
+alias mkdir='mkdir -p'
+alias now='date +'%T''
+alias nowdate='date +'%d-%m-%Y''
 alias jqcr='jq --compact-output --raw-output'
 alias javar='java -jar'
 alias g='gut'
