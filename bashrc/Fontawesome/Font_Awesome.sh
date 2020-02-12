1cd58d66c13c7d5b156ca6b2a5786ec1dfa576c6
quilicicf <quilicicf@gmail.com>
2020-02-12 16:48:31 +0100

    :new: Fix faPrint

--- bashrc/Fontawesome/Font_Awesome.sh
+++ bashrc/Fontawesome/Font_Awesome.sh
@@ -2,41 +2,14 @@
 # Font awesome #
 ################
 
-# FIXME: Broken ATM!
-
-# Displays the fontawesome icons corresponding to the provided search word.
-# Uses: y2j, jq, $FORGE
-# $1: the search word
-# $2: -s to search in strict mode
-# faSearch() {
-  # local input="$1"
-  # local strict icons_file
-  # strict="$(readVar "" "$2")"
-  # icons_file="$(find "$TOOLING/tmp" -name "fa_*_icons.json")"
-  #
-  # if [ "$strict" = "-s" ]; then
-  #   jqcr < "$icons_file" ".icons" \
-  #   | jsonSearch ".id" "\"$input\"" \
-  #   | jqcr '.unicode' \
-  #   | faPrintChar ""
-  #
-  # else
-  #   jqcr ".icons[]" < "$icons_file" | while read -r icon; do
-  #     if grepn "$input" <<< "$(jqcr '.id' <<< "$icon")"; then
-  #       jqcr ".unicode" <<< "$icon" | faPrintChar -n
-  #     fi
-  #   done
-  #   printf "\n"
-  # fi
-# }
-
-# Prints a character from its unicode.
+# Prints the characters that correspond to the search text.
+# Uses: $TOOLING
 # $1: the unicode
 # $2: -n to avoid writing a line break after the character
 faPrint() {
   local searchTerm="${1?Missing search term}"
   local iconsPath=~/.config/bashrc/fontawesome/icons.json
-  ./print.js "$iconsPath" "$searchTerm"
+  "$TOOLING/bashrc/Fontawesome/print.js" "$iconsPath" "$searchTerm"
 }
 
 # Updates the list of icons from the github repository.
