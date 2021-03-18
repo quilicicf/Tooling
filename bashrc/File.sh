015a88bfe91eeb46de0e13c3db47829213f2368a
quilicicf <cquilici@restlet.com>
2021-03-18 19:59:13 +0100

    :new: Add CRLF remover

--- bashrc/File.sh
+++ bashrc/File.sh
@@ -9,3 +9,16 @@ touchx() {
   chmod +x "$filePath"
   printf '%s' "$filePath"
 }
+
+# Remove CR in a file
+crlf2Lf() {
+  local file="${1?Missing file to update}"
+  [[ -f "$file" ]] || {
+    printf 'File %s does not exist\n' "$file"
+    return 1
+  }
+  
+  local content
+  content="$(<"$file")"
+  printf '%s' "${content//$'\r'/}" > "$file"
+}
