460c0e5620a126c30443190c170e4b4f6b6e0047
quilicicf <quilicicf@gmail.com>
2021-10-07 16:26:04 +0200

    :new: Compute `JAVA_HOME` dynamically

--- bashrc/4_Path.sh
+++ bashrc/4_Path.sh
@@ -15,3 +15,14 @@ export PATH="$PATH:$MAVEN/bin"
 export PATH="$PATH:/usr/local/go/bin"
 export GOPATH="$HOME/go"
 export PATH="$PATH:$GOPATH/bin"
+
+# Java
+java -version &> /dev/null && {
+  export JAVA_HOME
+  JAVA_HOME="$(
+    java -XshowSettings:properties -version 2>&1 \
+      | grep 'java.home' \
+      | awk -F '=' '{print $2}' \
+      | tr -d '[[:blank:]]'
+  )"
+}
