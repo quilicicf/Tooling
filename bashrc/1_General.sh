f7da8ee942ede21f8e37fa7606b9b96b63a493bb
quilicicf <quilicicf@gmail.com>
2022-01-03 18:29:05 +0100

    :art: Use Dracula with FZF

--- bashrc/1_General.sh
+++ bashrc/1_General.sh
@@ -79,9 +79,21 @@ fi
 # WHATEVER #
 ############
 
-fzfBinPath="$(asdf which fzf)"
-test -s "${fzfBinPath%/bin/fzf}/shell/completion.bash" && { source "$_"; }
-_fzf_setup_completion path micro xo cbs
+installFzf() {
+  fzfBinPath="$(asdf which fzf)"
+  test -s "${fzfBinPath%/bin/fzf}/shell/completion.bash" && { source "$_"; }
+  _fzf_setup_completion path micro xo cbs
+
+  local colors=(
+    '--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9'
+    '--color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9'
+    '--color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6'
+    '--color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
+  )
+  export FZF_DEFAULT_OPTS="${colors[*]}"
+}
+
+installFzf
 
 # Parses a CLI's help from stdin and greps the documentation for 
 # one of its parameters out of it.
