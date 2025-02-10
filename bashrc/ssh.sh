fc836d5e680615d7d8df93305e473eebc83088e9
quilicicf <quilicicf@gmail.com>
2025-02-10 10:11:41 +0100

    :new: Complete `ssh` command with hosts from `~/.ssh/config`

--- bashrc/ssh.sh
+++ bashrc/ssh.sh
@@ -27,3 +27,24 @@ sshTunnelBdd() (
   # -L Port forwarding
   ssh -fNg -L "${localPort}:127.0.0.1:${remotePort}" "${remoteAlias}" "$@"
 )
+
+_sshComplete() 
+{
+  local cur prev opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  opts="$(
+    grep '^Host' ~/.ssh/config ~/.ssh/config.d/* 2>/dev/null \
+      | grep -v '[?*]' \
+      | cut -d ' ' -f 2- \
+      | while read -r line; do 
+        splitLine=($line);
+        printf '%s\n' "${splitLine[@]}";
+      done
+  )"
+
+  COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+  return 0
+}
+complete -F _sshComplete ssh
